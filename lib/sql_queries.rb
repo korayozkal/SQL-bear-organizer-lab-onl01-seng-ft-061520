9ed80801cdc1cb3a567dfd5a82b60b0d9d51fb3e
Koray Ozkal <koryozkal@gmail.com>
2020-07-14 02:33:01 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -16,16 +16,16 @@ def selects_oldest_bear_and_returns_name_and_age
 end
 
 def select_youngest_bear_and_returns_name_and_age
-   "SELECT name, MIN(age) FROM bears"
+   "SELECT name, MIN(age) FROM bears";
 end
 
 def selects_most_prominent_color_and_returns_with_count
-"SELECT color, COUNT(color) FROM bears GROUP BY color ORDER BY COUNT(*) DESC LIMIT 1"
+"SELECT color, COUNT(color) FROM bears GROUP BY color ORDER BY COUNT(*) DESC LIMIT 1";
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-"SELECT COUNT(temperament) FROM bears WHERE temperament = 'goofy' "
+"SELECT COUNT(temperament) FROM bears WHERE temperament = 'goofy' "; 
 
 def selects_bear_that_killed_Tim
-"SELECT * FROM bears WHERE name IS NULL"
+"SELECT * FROM bears WHERE name IS NULL";
 end
